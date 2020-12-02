6998c8d18714986d5991f45b583c8cc5e32ce514
blfuentes <epiphonetodie@gmail.com>
2020-12-02 13:34:15 +0100

    added explicit datatype to xor operator

--- Modules/Utilities.fs
+++ Modules/Utilities.fs
@@ -15,5 +15,5 @@ let rec combination (num: int, list: List<'T>) : List<List<'T>> =
     | k, (x::xs) -> List.map ((@) [x]) (combination ((k-1), xs)) @ (combination (k, xs))
 
 // XOR OPERATOR
-let (^@) a b =
+let (^@) (a: bool) (b:bool) : bool =
     a <> b
