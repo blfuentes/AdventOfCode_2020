1f73f5c6b835229dca62bf48aedbb44e0ca71440
blfuentes <insanespeech@hotmail.es>
2020-12-14 15:04:54 +0100

    working on day 11

--- day11/part01/day11_part01.fsx
+++ day11/part01/day11_part01.fsx
@@ -7,8 +7,8 @@
 open Utilities
 open CustomDataTypes
 
-let file = "test_input.txt"
-//let file = "day11_input.txt"
+//let file = "test_input.txt"
+let file = "day11_input.txt"
 let path = __SOURCE_DIRECTORY__ + @"../../" + file
 let inputLines = GetLinesFromFileFSI2(path)
 
@@ -19,62 +19,63 @@ let placesDict = new Dictionary<int*int, SeatInfo>()
 
 let plane =
     seq {
-        for idx in [0..maxX - 1] do
-            for jdx in [0..maxY - 1] do
+        for jdx in [0..maxY - 1] do
+            for idx in [0..maxX - 1] do
                 let seat =
                     match inputLines.[jdx].[idx] with
                     | '.' -> SeatStatus.FLOOR
                     | 'L' -> SeatStatus.EMPTY
                     | '#' -> SeatStatus.OCCUPIED
                     | _ -> failwith("wrong site type")
-                let seatToAdd = { Seat = seat; Location = (idx, jdx); Content = inputLines.[jdx].[idx] }
-                placesDict.Add((idx, jdx), seatToAdd)
+                let seatToAdd = { Seat = seat; Location = (jdx, idx); Content = inputLines.[jdx].[idx] }
+                if seatToAdd.Seat <> SeatStatus.FLOOR then
+                    placesDict.Add((jdx, idx), seatToAdd)
                 yield seatToAdd
     } |> List.ofSeq
 
-let printPlane (planeToPrint: SeatInfo list) (dictPlane: Dictionary<(int*int), SeatInfo>)= 
-    for idx in [0 .. maxX - 1] do
-        for jdx in [0 .. maxY - 1] do
-            //match placesDict((idx, jdx), )
-            printf "%c" placesDict.[(jdx, idx)].Content
+let printPlane (dictPlane: Dictionary<(int*int), SeatInfo>)= 
+    for idx in [0 .. maxY - 1] do
+        for jdx in [0 .. maxX - 1] do
+            if dictPlane.ContainsKey(idx, jdx) then
+                printf "%c" dictPlane.[(idx, jdx)].Content
+            else
+                printf "."
         printfn ""
 
-let printPlane2 (planeToPrint: SeatInfo list)= 
-    for seat in planeToPrint do
-        if snd seat.Location = (maxY - 1) then
-            printfn ""
-        printf "%c" seat.Content
+let getNewPlaces (places: Dictionary<(int*int), SeatInfo>) =
+    let newplacesDict = new Dictionary<int*int, SeatInfo>()
+    for place in places do      
+        let seatsToCheck =
+            seq {
+                for idx in [(fst place.Value.Location) - 1 .. (fst place.Value.Location) + 1] do
+                    for jdx in [(snd place.Value.Location) - 1 .. (snd place.Value.Location) + 1] do
+                        if places.ContainsKey(idx, jdx) && (fst place.Value.Location <> idx || snd place.Value.Location <> jdx) then
+                            yield places.[(idx, jdx)]
+            } |> List.ofSeq
+        let adjOccupied = seatsToCheck |> List.filter(fun s -> s.Seat = SeatStatus.OCCUPIED) |> List.length
+        let newSeat = 
+            match place.Value.Seat with
+            | SeatStatus.EMPTY -> 
+                if adjOccupied = 0 then
+                    { Seat= SeatStatus.OCCUPIED; Location= (place.Value.Location); Content= '#'}
+                else
+                    { Seat= place.Value.Seat; Location= (place.Value.Location); Content= place.Value.Content } 
+            | SeatStatus.OCCUPIED -> 
+                if adjOccupied > 3 then
+                    { Seat= SeatStatus.EMPTY; Location= (place.Value.Location); Content= 'L'}
+                else
+                    { Seat= place.Value.Seat; Location= (place.Value.Location); Content= place.Value.Content } 
+            | _ -> place.Value
+        newplacesDict.Add((fst place.Value.Location, snd place.Value.Location), newSeat)
+    newplacesDict
 
-let emptySeats = plane |> List.filter(fun s -> s.Seat = SeatStatus.EMPTY)
-let occupiedSeats = plane |> List.filter(fun s -> s.Seat = SeatStatus.OCCUPIED)
+let rec round (oldplaces: Dictionary<(int*int), SeatInfo>) (rounds: int) =
+    let newplacesDict = getNewPlaces oldplaces
+    printfn "Round %i" rounds
+    //printPlane newplacesDict
+    let planesAreIdentical = oldplaces.Values |> List.ofSeq |> List.forall(fun op -> newplacesDict.[(fst op.Location, snd op.Location)].Seat = op.Seat)
+    match planesAreIdentical with
+    | true -> newplacesDict |> List.ofSeq |> List.filter(fun s -> s.Value.Seat = SeatStatus.OCCUPIED) |> List.length
+    | false -> round newplacesDict (rounds + 1)
 
-let getNewStatus (cheackPlane: SeatInfo list) (seat: SeatInfo) =
-    let seatsToCheck =
-        seq {
-            for idx in [(fst seat.Location) - 1 .. (fst seat.Location) + 1] do
-                for jdx in [(snd seat.Location) - 1 .. (snd seat.Location) + 1] do
-                    if fst seat.Location <> idx && snd seat.Location <> jdx then
-                        yield (idx, jdx)
-        } |> List.ofSeq
-    let adjEmpty = cheackPlane |> List.filter(fun s -> (seatsToCheck |> List.exists(fun c -> (fst c = fst s.Location) && (snd c = snd s.Location))) && s.Seat = SeatStatus.EMPTY)
-    let adjOccupied = cheackPlane |> List.filter(fun s -> (seatsToCheck |> List.exists(fun c -> (fst c = fst s.Location) && (snd c = snd s.Location))) && s.Seat = SeatStatus.OCCUPIED)
-    match seat.Seat with
-    | SeatStatus.EMPTY -> 
-        if adjEmpty.Length = 0 then
-            { Seat= SeatStatus.OCCUPIED; Location= (seat.Location); Content= '#'}
-        else
-            { Seat= seat.Seat; Location= (seat.Location); Content= seat.Content } 
-    | SeatStatus.OCCUPIED -> 
-        if adjOccupied.Length > 3 then
-            { Seat= SeatStatus.EMPTY; Location= (seat.Location); Content= 'L'}
-        else
-            { Seat= seat.Seat; Location= (seat.Location); Content= seat.Content } 
-    | _ -> seat
-
-
-//printPlane plane placesDict
-printPlane2 plane
-let newPlane = plane |> List.map (fun s -> getNewStatus plane s)
-printPlane2 newPlane
-//let rec StatusChanged initPlane eSeats oSeats=
-//    le
+round placesDict 0
